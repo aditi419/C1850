604200c00f77abcb0d293f6457296d683796c2e1
aditi419 <76972940+aditi419@users.noreply.github.com>
2022-01-15 15:57:51 -0500

    Add files via upload

---
+++ C185.py
@@ -0,0 +1,27 @@
+import hashlib
+from simplecrypt import encrypt, decrypt
+value = 'Aditi: Hello'
+def SHA256():
+    result = hashlib.sha256(value.encode())
+    print('SHA256 encrypted data: ', result.hexdigest())
+SHA256()
+def MD5():
+    result = hashlib.md5(value.encode())
+    print('MD5 encrypted data: ', result.hexdigest())
+MD5()
+message = 'Aditi: Hello'
+hex_string = ''
+def encryption():
+    global hex_string
+    ciphercode = encrypt('AIM', message)
+    hex_string = ciphercode.hex()
+    print('Encrypted String', hex_string)
+def decryption():
+    global hex_string
+    byte_str = bytes.fromhex(hex_string)
+    original = decrypt('AIM', byte_str)
+    final_message = original.decode('utf-8')
+    print('Decryption', final_message)
+encryption()
+decryption()
+
